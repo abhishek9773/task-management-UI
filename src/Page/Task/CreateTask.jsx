e15a1e41c5908c556cee5afb5603e93dcdd51b00
anand9773 <aaanand9773@gmail.com>
2024-04-28 18:17:20 +0530

    working on createTask.jsx

--- src/Page/Task/CreateTask.jsx
+++ src/Page/Task/CreateTask.jsx
@@ -3,6 +3,11 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { useState } from "react";
+import { Autocomplete, Grid, TextField } from "@mui/material";
+import { Description } from "@mui/icons-material";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const style = {
   position: "absolute",
@@ -16,7 +21,70 @@ const style = {
   p: 4,
 };
 
+const tags = ["Angular", "React", "VueJs", "spring boot", "nodejs", "python"];
 export default function CreateNewTaskFrom({ open, handleClose }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    image: "",
+    description: "",
+    tags: [],
+    deadline: new Date(),
+  });
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleTagsChange = (event, value) => {
+    setSelectedTags(value);
+  };
+
+  const handleDeadlineChange = (date) => {
+    setFormData({
+      ...formData,
+      deadline: date,
+    });
+  };
+
+  const formateDate = (input) => {
+    let {
+      $y: year,
+      $M: month,
+      $D: day,
+      $H: hours,
+      $m: minutes,
+      $s: seconds,
+      $ms: milliseconds,
+    } = input;
+
+    const date = new Date(
+      year,
+      month,
+      day,
+      hours,
+      minutes,
+      seconds,
+      milliseconds
+    );
+
+    const formatedDate = date.toISOString();
+    return formatedDate;
+  };
+
+  const handleSubmit = (e) => {
+    e.prevenDefault();
+    const { deadline } = formData;
+    formData.deadline = formateDate(deadline);
+    formData.tags = selectedTags;
+
+    console.log("formData", formData);
+    handleClose();
+  };
   return (
     <div>
       <Modal
@@ -26,8 +94,59 @@ export default function CreateNewTaskFrom({ open, handleClose }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <form>
-            <Grid></Grid>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={12}>
+                <TextField
+                  lable="Title"
+                  fullWidth
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  lable="Image"
+                  fullWidth
+                  name="image"
+                  value={formData.image}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Autocomplete
+                  onChange={handleTagsChange}
+                  getOptionLabel={(option) => option}
+                  renderInput={(params) => (
+                    <TextField lable="Tags" fullWidth {...params} />
+                  )}
+                  multiple
+                  id="multiple=limit-tags"
+                  options={tags}
+                />
+              </Grid>
+              <Grid item sx={12}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    onChange={handleDeadlineChange}
+                    renderInput={(params) => <TextField {...params} />}
+                    className="w-full"
+                    label="Deadline"
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item sx={12}>
+                <Button
+                  fullWidth
+                  className="customButton"
+                  type="submit"
+                  sx={{ padding: ".9rem" }}
+                >
+                  Create
+                </Button>
+              </Grid>
+            </Grid>
           </form>
         </Box>
       </Modal>
